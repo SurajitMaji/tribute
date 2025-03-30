f02bee252cc9c300f0e6c53142c995a9b6b7e9bf
SurajitMaji <132469890+SurajitMaji@users.noreply.github.com>
2025-03-30 10:42:51 +0530

    Add files via upload

---
+++ ghibli.js
@@ -0,0 +1,14 @@
+const images = [
+    '2eb3ac41-1eba-48c7-b145-421c6047feb3.png',
+    'image3.jpg',
+    'insidetrain.png'
+];
+
+const ghibli_area = document.getElementById('ghibli_area');
+
+images.forEach(image => {
+    const imgElement = document.createElement('img');
+    imgElement.src = `ghibli/${image}`; // Update the path to your images
+    imgElement.alt = image.split('.')[0]; // Use the image name without extension as alt text
+    ghibli_area.appendChild(imgElement);
+});
